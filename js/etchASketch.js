ac691375da9ce80123e444f8a860a2e6a25ebc96
Athanasios Grivas <nasos.grivas@gmail.com>
2022-12-17 23:01:32 +0200

    Add a button to insert number of squares per side

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -1,22 +1,37 @@
 let wrapperDiv = document.createElement("div");
+const buttonPrompt = document.querySelector("#insert-num-squares");
+const container = document.querySelector("#container");
+let squareNum;
 
-const body = document.querySelector("body");
-const script = document.querySelector("script");
+buttonPrompt.addEventListener("click", () => {
+  const regex = /^[0-9]+$/;
+  let input = prompt("How many squares per side?");
+
+  while (!input.match(regex)) {
+    input = prompt("Number needs to be an integer!");
+  }
+  squareNum = parseInt(input);
+  while (squareNum < 1 || squareNum > 100) {
+    squareNum = parseInt(
+      prompt("Number needs to be an integer from 1 to 100!")
+    );
+  }
+});
 
 wrapperDiv.className = "wrapper";
-body.insertBefore(wrapperDiv, script);
+container.appendChild(wrapperDiv);
 
 for (let i = 0; i < 256; i++) {
   let squareDiv = document.createElement("div");
   squareDiv.className = "square";
   wrapperDiv.appendChild(squareDiv);
-  squareDiv.addEventListener("mouseover", () => {
-    squareDiv.classList.add("hover");
-  })
-  squareDiv.addEventListener("mouseout", () => {
-    squareDiv.classList.remove("hover");
-  })
+  squareDiv.addEventListener("mouseover", () =>
+    squareDiv.classList.add("hover")
+  );
+  squareDiv.addEventListener("mouseout", () =>
+    squareDiv.classList.remove("hover")
+  );
 }
 
 wrapperDiv.style.gridTemplateColumns = "repeat(16, 1fr)";
-wrapperDiv.style.gridAutoRows = "40px";
+wrapperDiv.style.gridAutoRows = "25px";
